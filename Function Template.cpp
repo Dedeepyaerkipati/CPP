c6be03d7f72d9271af62df4c616300e1c8ee9887
Dedeepyaerkipati <dedeepyaerkipati@gmail.com>
2025-11-06 20:47:42 +0530

    Add files via upload

---
+++ Function Template.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+template<class T>
+T add(T a, T b)
+{
+	return a+b;
+	
+}
+int main()
+{
+	cout <<"integers:" << add(2,3) << endl;
+	cout <<"float:" << add(2.3,4.5)<< endl;
+	cout <<"characters:" <<add('a',(char)2) << endl;
+	}
